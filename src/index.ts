95d004e1fe1bf4495f2314f8cf30b47d957e4710
Lara <larysa.yavorska@outlook.com>
2021-03-08 00:25:05 +0100

    feat: add error handling for countries id endpoint

--- src/index.ts
+++ src/index.ts
@@ -64,41 +64,46 @@ const apiServer = express();
   });
 
   apiServer.get("/countries/:id", async (request, response) => {
-    const lang = getLanguageFromRequest(request);
-    const objectId = new ObjectID(request.params.id);
+    try {
+      const lang = getLanguageFromRequest(request);
+      const objectId = new ObjectID(request.params.id);
 
-    const { _id, name, image, galleryImages, description } = await db
-      .db("travelapp")
-      .collection<CountryDBObject>("countries")
-      .findOne(objectId);
+      const { _id, name, image, galleryImages, description } = await db
+        .db("travelapp")
+        .collection<CountryDBObject>("countries")
+        .findOne(objectId);
 
-    const id = _id.toHexString();
-    const nameLang = name[lang];
-    const imageLang = {
-      ...image,
-      alt: image.alt[lang],
-    };
-    const galleryImagesLang = galleryImages.map(
-      ({ alt, description, ...items }) => {
-        const altLang = alt[lang];
-        const descriptionLang = description[lang];
-        return {
-          ...items,
-          alt: altLang,
-          description: descriptionLang,
-        };
-      }
-    );
-    const descriptionLang = description[lang];
-    const responseData = {
-      id,
-      name: nameLang,
-      image: imageLang,
-      galleryImages: galleryImagesLang,
-      description: descriptionLang,
-    };
+      const id = _id.toHexString();
+      const nameLang = name[lang];
+      const imageLang = {
+        ...image,
+        alt: image.alt[lang],
+      };
+      const galleryImagesLang = galleryImages.map(
+        ({ alt, description, ...items }) => {
+          const altLang = alt[lang];
+          const descriptionLang = description[lang];
+          return {
+            ...items,
+            alt: altLang,
+            description: descriptionLang,
+          };
+        }
+      );
+      const descriptionLang = description[lang];
+      const responseData = {
+        id,
+        name: nameLang,
+        image: imageLang,
+        galleryImages: galleryImagesLang,
+        description: descriptionLang,
+      };
 
-    response.json(responseData);
+      response.json(responseData);
+    } catch ({ message }) {
+      console.log(`Endpoint (/countries/:id) error: ${message}`);
+      response.status(406).send(message);
+    }
   });
 
   apiServer.listen(PORT, () => {
